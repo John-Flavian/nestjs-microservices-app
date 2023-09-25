3684f0afa5bb5c4f139b82ca710784a53ff98c35
John-Flavian <flavianomeokwe@gmail.com>
2023-09-25 17:15:50 +0100

    feat: Adds the basic settings for the project route.

--- src/project/project.controller.ts
+++ src/project/project.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { ProjectService } from './project.service';
+import { GetUser } from 'src/auth/decorator';
+import { User } from '@prisma/client';
+import { JwtGuard } from 'src/auth/guard';
 
-@Controller('project')
-export class ProjectController {}
+@UseGuards(JwtGuard)
+@Controller('projects')
+export class ProjectController {
+  constructor(private projectService: ProjectService) {}
+
+  @Get()
+  getProjects(@GetUser() user: User) {
+    return `Projects gotten:, ${user.id}`;
+  }
+}
